fa08f38782c0a0709cff4d30af9240751ad6e0f1
tomimarkus991 <tomimarkusalber@gmail.com>
2023-04-13 14:53:47 +0300

    init commit

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,8 @@
-const title = 'React Three Next Starter'
+const title = 'Test 3D NextJS'
 const url = 'https://react-three-next.vercel.app/'
 const description = 'The easiest and fastest way to create a 3D website using React Three Fiber and NextJS'
-const author = 'Author'
-const twitter = '@pmndrs'
+const author = 'Galaxy'
+const twitter = '@galaxy'
 
 export default function Head() {
   return (
